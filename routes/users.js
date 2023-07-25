9150168d571f3eb1eb4ccfaf21620c6c547ffdb9
jaini <jainisjustino@gmail.com>
2023-07-25 11:10:34 -0300

    beachifc

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users cool. */
+router.get('/cool', function(req, res, next) {
+ // res.render('index', { title: 'you are so cool' });
+ res.send('you are so cool');
+});
+
+/* GET users cool. */
+router.get('/cool/beach', function(req, res, next) {
+  // res.render('index', { title: 'praia!' });
+  res.send('Lets go to the beach');
+ });
+
+ /* GET users cool. */
+router.get('/ifc', function(req, res, next) {
+  // res.render('index', { title: 'Bem vindo!' });
+  res.send('Bem vindo ao IFC, mini querido');
+ });
+
 module.exports = router;
